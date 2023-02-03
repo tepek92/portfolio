ceeb20ef60482276c3715513ad0a1369f5ef6e30
tepek92 <tepek92@mail.ru>
2023-02-03 15:28:45 +0700

    fix(Project): description project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,8 @@ import Fade from 'react-reveal/Fade';
 
 const TD_DESCRIPTION = 'Classic task manager. Implemented login and logout. Creation of several todolists, removal. You can add tasks, mark completed tasks, and filter tasks by status.'
 const SN_DESCRIPTION = 'Preference any astonished unreserved mrs. Prosperous understood middletons in conviction an uncommonly do. Supposing so be resolving breakfast am or perfectly. Is drew am hill from mr.'
-const CARDS_DESCRIPTION = `When we collect the answer ourselves and generate it, the brain works more actively. This principle is the basis of learning with cards. We see a question - one side of the card with an unfamiliar word or definition. We then try to find the answer on our own and, if we fail, we open the back side and check ourselves. You can create your own study cards in the app and share them with other students. You can decorate the cards with pictures.`
+const CARDS_DESCRIPTION = `Create learning cards and learn from them.
+You can create your own cards by adding pictures to them, as well as search for cards created by other users.`
 
 const cardsStack = [
     'typescript',
