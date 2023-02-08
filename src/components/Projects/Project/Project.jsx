ce25cba774e6edeb8002c295c0d82963332300c4
tepek92 <tepek92@mail.ru>
2023-02-08 15:10:40 +0700

    refactor(Project): description

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -1,6 +1,8 @@
 import style from './Project.module.scss';
 
 export const Project = ({title, description, imgProject, link, stack}) => {
+    const descriptionList = description.map(el => <li key={el}>{el}</li>)
+
     return (
         <div className={style.project}>
             <div className={style.imgBox}>
@@ -8,7 +10,10 @@ export const Project = ({title, description, imgProject, link, stack}) => {
             </div>
             <div className={style.textBox}>
                 <h3 className={style.title}>{title}</h3>
-                <p className={style.description}>{description}</p>
+                <span className={style.description}>Implemented:</span>
+                <ul className={style.description}>
+                    {descriptionList}
+                </ul>
                 <p className={style.stack}>Used stack:</p>
                 <ul className={style.tags}>
                     {stack.map(s => <li key={s}>{s}</li>)}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,15 +2,46 @@ import style from './Projects.module.scss';
 import styleContainer from '../../common/styles/Container.module.css'
 import {Project} from "./Project/Project";
 import {Title} from "../../common/components/Title/Title";
-import socialImg from "../../assets/image/social_network4.webp"
+import socialImg from "../../assets/image/socialnetwork.png"
 import todoImg from "../../assets/image/Todolist.png"
 import cardImg from "../../assets/image/Cards.png"
 import Fade from 'react-reveal/Fade';
 
-const TD_DESCRIPTION = 'Classic task manager. Implemented login and logout. Creation of several todolists, removal. You can add tasks, mark completed tasks, and filter tasks by status.'
-const SN_DESCRIPTION = 'In this project, I worked with class components and some old libraries. What can be useful in projects with legacy code.'
-const CARDS_DESCRIPTION = `Create learning cards and learn from them.
-You can create your own cards by adding pictures to them, as well as search for cards created by other users.`
+// const TD_DESCRIPTION = 'Classic task manager. Implemented login and logout. Creation of several todolists, removal. You can add tasks, mark completed tasks, and filter tasks by status.'
+// const SN_DESCRIPTION = 'In this project, I worked with class components and some old libraries. What can be useful in projects with legacy code.'
+// const CARDS_DESCRIPTION = `Create learning cards and learn from them.
+// You can create your own cards by adding pictures to them, as well as search for cards created by other users.`
+
+
+const TD_DESCRIPTION = [
+    'login / logout ',
+    'creating and deleting todolists',
+    'creating and deleting tasks ',
+    'flag completed ',
+    'task filtering ',
+    'editing the heading of todolists and tasks',
+]
+
+const SN_DESCRIPTION = [
+    'login / logout / captcha',
+    'profile editing',
+    'publishing posts',
+    'pagination on the user page',
+    'user subscription and unsubscription',
+    'go to user page',
+    'sending messages',
+]
+
+const CARDS_DESCRIPTION = [
+    'login / registration / password recovery',
+    'filtering and searching in tables',
+    'pagination in tables',
+    'adding and removing packs/cards',
+    'adding pictures to questions',
+    'card learning process',
+    'profile editing',
+    'server error handling',
+]
 
 const cardsStack = [
     'typescript',
@@ -33,6 +64,14 @@ const todolistStack = [
     'formik'
 ]
 
+const socialNetworkStack = [
+    'typescript',
+    'react',
+    'redux',
+    'react-router',
+    'material-ui',
+    'redux-form'
+]
 
 
 export const Projects = () => {
@@ -59,9 +98,9 @@ export const Projects = () => {
                             link={"https://github.com/tepek92/todolist"}
                         />
                         <Project
-                            title={"Social Network web app for you"}
+                            title={"Social Network"}
                             description={SN_DESCRIPTION}
-                            stack={cardsStack}
+                            stack={socialNetworkStack}
                             imgProject={socialImg}
                             link={"https://github.com/tepek92/samurai_way_ts"}
                         />
